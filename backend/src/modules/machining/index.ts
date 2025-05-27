692b5d46a88da7d4dcbe9ebee221206c20ca2a5c
Milan Lakhani <milanlakhaniaiml@gmail.com>
2025-05-26 21:36:12 -0400

    fix: deleting machining logic

--- backend/src/modules/machining/index.ts
+++
@@ -1,8 +0,0 @@
-import MachiningModuleService from "./service"
-import { Module } from "@medusajs/framework/utils"
-
-export const MACHINING_SERVICE_MODULE = "machinings"
-
-export default Module(MACHINING_SERVICE_MODULE, {
-  service: MachiningModuleService,
-})
